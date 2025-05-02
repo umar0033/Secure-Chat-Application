19cfca48b87dea065d7d2b95a895e63966336110
SyedMuhammadAbuBakar <abubakarsyed592@gmail.com>
2025-05-02 06:52:19 +0500

    Update User.java

--- src/main/java/com/example/cn_project/model/User.java
+++ src/main/java/com/example/cn_project/model/User.java
@@ -3,7 +3,7 @@ package com.example.cn_project.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
-import java.util.List;
+///import java.util.List;
 
 @Data
 @Document(collection = "users")
@@ -13,5 +13,5 @@ public class User {
     private String username;
     private String password;
     private String email;
-    private List<Message> sentMessages;
+    /*private List<Message> sentMessages; to avoid messaging embedding...collection got too large*/
 }
