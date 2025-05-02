c2f302d7e7b82849a13366b0979f24b63cb1efe9
SyedMuhammadAbuBakar <abubakarsyed592@gmail.com>
2025-05-02 06:51:50 +0500

    Update Message.java

--- src/main/java/com/example/cn_project/model/Message.java
+++ src/main/java/com/example/cn_project/model/Message.java
@@ -3,20 +3,22 @@ package com.example.cn_project.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "messages")
 public class Message {
     @Id
     private String id;
+    
     private String content;
-    private LocalDateTime timestamp;
+    private LocalDateTime timestamp = LocalDateTime.now();
     private String senderId;
     private String receiverId;
+    private boolean read = false;
     
-    // Constructor
-    public Message() {
-        this.timestamp = LocalDateTime.now();
-    }
+    // Reference to which folder this message belongs to
+    private String folderId;
 }
