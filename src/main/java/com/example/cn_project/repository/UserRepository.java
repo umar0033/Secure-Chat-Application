e74c0c535c808b1d70162f9697c3180a9a087304
SyedMuhammadAbuBakar <abubakarsyed592@gmail.com>
2025-05-02 06:58:18 +0500

    Update UserRepository.java

--- src/main/java/com/example/cn_project/repository/UserRepository.java
+++ src/main/java/com/example/cn_project/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.cn_project.model.User;  // Add this import
 
 public interface UserRepository extends MongoRepository<User, String> {
-    // Your repository methods here
+    public String findByUsername(String username);
     
 }
